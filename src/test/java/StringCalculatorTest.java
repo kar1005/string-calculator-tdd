e024fd8fb4217a113e83e8b9c548be429ec82d05
Khushi <khushiruparelia2004@gmail.com>
2025-07-04 23:37:33 +0530

    adding testcase for single number input

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,12 @@
 import org.incubyte.StringCalculator;   
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 class StringCalculatorTest {
    StringCalculator calculator = new StringCalculator();
+
+   @Test
+    void shouldReturnNumberForSingleInput() {
+        assertEquals(5, calculator.add("5"));
+    }
+
 }
