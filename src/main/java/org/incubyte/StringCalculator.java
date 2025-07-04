2b43737f98595a8ed351eac5a431fc6fca220dbc
Khushi <khushiruparelia2004@gmail.com>
2025-07-04 23:41:38 +0530

    Implementing single number input support

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -2,6 +2,7 @@ package org.incubyte;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0; // Initial stub
+        if (numbers.isEmpty()) return 0;
+        return Integer.parseInt(numbers);
     }
 }
