b1509d2768e5b95940dfddf7c949971e0ff7b29b
Khushi <khushiruparelia2004@gmail.com>
2025-07-04 23:55:55 +0530

    Code for adding 2 numbers

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -3,6 +3,10 @@ package org.incubyte;
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
+        if (numbers.contains(",")) {
+            String[] parts = numbers.split(",");
+            return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
+        }
         return Integer.parseInt(numbers);
     }
 }
