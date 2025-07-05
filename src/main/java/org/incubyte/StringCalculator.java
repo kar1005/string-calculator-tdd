9e82bfe036c332c0ad35c173cc802601e9fcfac4
Khushi <khushiruparelia2004@gmail.com>
2025-07-05 15:43:11 +0530

    Add compatibity for new line test case

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
 
-        String[] tokens = numbers.split(",");
+        String[] tokens = numbers.split(",|\n");
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token.trim());
