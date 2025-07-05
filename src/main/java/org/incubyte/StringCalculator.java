b3b1b9390ca9c0544b9eca79e755d45397168100
Khushi <khushiruparelia2004@gmail.com>
2025-07-05 16:09:08 +0530

    Code to manage custom delimiter

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -4,11 +4,19 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
 
-        String[] tokens = numbers.split(",|\n");
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            String[] parts = numbers.split("\n", 2);
+            delimiter = parts[0].substring(2);
+            numbers = parts[1];
+        }
+
+        String[] tokens = numbers.split(delimiter);
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token.trim());
         }
         return sum;
     }
+
 }
